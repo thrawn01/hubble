db74d267adcdbafb09ec428bf19d1bd02a4feca8
Derrick J. Wippler <thrawn01@gmail.com>
2014-03-11 11:02:50 -0500

    Converted setup.py to use pbr

--- setup.py
+++ setup.py
@@ -1,41 +1,5 @@
-#   Copyright 2012 Derrick J. Wippler
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
-
 import setuptools
 
-
 setuptools.setup(
-    name='hubble',
-    version='0.1',
-    author='Derrick J. Wippler',
-    author_email='thrawn01@gmail.com',
-    description='openstack environment manager',
-    license='Apache License, Version 2.0',
-    url='https://github.com/thrawn01/hubble',
-    packages=['hubble'],
-    entry_points={
-        'console_scripts': [
-            'hubble = hubble.shell:main',
-            'hubble-keyring = hubble.keys:main',
-        ]
-    },
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Information Technology",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-    ]
-)
+    setup_requires=['pbr'],
+    pbr=True)
